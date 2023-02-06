7b0fd94081b226d6398fac17eb6a15177fb55b44
lissalad <elissalaymon@gmail.com>
2023-02-06 12:11:00 -0800

    everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [pepperoni, setPepperoni] = useState(false);
+  const [sausage, setSausage] = useState(false);
+  const [peppers, setPeppers] = useState(false);
+  const [onions, setOnions] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <form>
+        <h1>Pizza Order</h1>
+        <input
+          type="text"
+          placeholder="name"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <div>
+          <div>
+            <input
+              type="checkbox"
+              checked={pepperoni}
+              onChange={() => setPepperoni(!pepperoni)}
+            />
+            <label>Pepperoni</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              checked={sausage}
+              onChange={() => setSausage(!sausage)}
+            />
+            <label>Sausage</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              checked={peppers}
+              onChange={() => setPeppers(!peppers)}
+            />
+            <label>Peppers</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              checked={onions}
+              onChange={() => setOnions(!onions)}
+            />
+            <label>Onions</label>
+          </div>
+        </div>
+        <button>submit</button>
+      </form>
+      <div>
+        {name ? <h2>{name}'s order</h2> : <h2>your order</h2>}
+        <div>{pepperoni && <div>Pepperoni</div>}</div>
+        <div>{sausage && <div>Sausage</div>}</div>
+        <div>{peppers && <div>Peppers</div>}</div>
+        <div>{onions && <div>Onions</div>}</div>
+      </div>
     </div>
   );
 }
